73ed9dfe377e15f2a0694109f2e6a51abef9af12
irynaprofile <105556785+irynaprofile@users.noreply.github.com>
2023-02-23 19:08:49 -0600

    studio 11

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -4,11 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("user")
 public class UserController {
-    @RequestMapping("user")
     public String displayAddUserForm() {
-
         return "/user/add";
 
 }
+
+
 }
